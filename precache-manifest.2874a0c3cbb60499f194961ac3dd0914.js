83031f6f5a1ecfd9da4257b20b5cc5900dac7e07
Gautam2404 <barfagautam@gmail.com>
2020-08-10 16:00:28 +0530

    Updates

---
+++ precache-manifest.2874a0c3cbb60499f194961ac3dd0914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a074e4ce8df8c86d3439743e51308f8d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "4be233acbc28bf812722",
+    "url": "/react-todo-app/static/css/main.e1a4f92d.chunk.css"
+  },
+  {
+    "revision": "4f60acfdb80203d29f63",
+    "url": "/react-todo-app/static/js/2.55bd0e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.55bd0e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be233acbc28bf812722",
+    "url": "/react-todo-app/static/js/main.d4c16cba.chunk.js"
+  },
+  {
+    "revision": "30c5a76aa92b2f359353",
+    "url": "/react-todo-app/static/js/runtime-main.b74522dc.js"
+  }
+]);
